ea6b85cdecf4d3920bc13b2a2f8ee2bd9dc04da9
Samuel R. <samuel_-_rojas@hotmail.com>
2024-05-11 00:42:53 -0500

    refactor: Remove unused helper functions and interfaces

--- src/shared/interfaces/command.ts
+++
@@ -1,8 +0,0 @@
-import { Argv, Options } from 'yargs';
-
-export interface Command {
-  command: string;
-  description: string;
-  options: { [key: string]: Options };
-  handler: (yargs: Argv) => void;
-}
