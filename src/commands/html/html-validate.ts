402e1b5e6db7c79da37ca42e4c009e7c332a11cb
Samuel R. <samuel_-_rojas@hotmail.com>
2024-05-10 20:58:41 -0500

    refactor: Update HTMLHint dependencies and fix import paths in html-validate.ts

--- src/commands/html/html-validate.ts
+++ src/commands/html/html-validate.ts
@@ -1,79 +1,87 @@
-import { Argv, InferredOptionTypes } from 'yargs';
-import { cwd } from 'process';
-import { File } from '@/shared/lib/file';
-import { Hint, Ruleset } from 'htmlhint/types';
-import { HTMLHint } from 'htmlhint';
-import { log, warn } from '@/shared/helpers/console';
-import { Notify } from '@/shared/lib/notify';
-import config from '@/shared/helpers/config';
-import { Command } from '@/shared/interfaces/command';
+import { cwd, uptime, exit } from 'node:process'
 
-const htmlhintStylish = require('htmlhint-stylish');
+import type { ArgumentsCamelCase, InferredOptionTypes } from 'yargs'
+import { HTMLHint } from 'htmlhint'
+import htmlhintStylish from 'htmlhint-stylish'
 
-class HtmlValidate implements Command {
-  readonly command = 'html:validate';
+import { File } from '#@/src/shared/lib/file.js'
+import { log, warn } from '#@/src/shared/helpers/console.js'
+import { Notify } from '#@/src/shared/lib/notify.js'
+import config from '#@/src/shared/helpers/config.js'
+import { YargsCommand } from '#@/src/shared/yargs-command.js'
+
+type Ruleset = (typeof HTMLHint)['defaultRuleset']
+type Hint = ReturnType<(typeof HTMLHint)['verify']>[number]
+
+class HtmlValidate extends YargsCommand {
+  readonly command = 'html:validate'
   readonly description =
-    'Validate html files with htmlhint. \nRead about rules on:\n- https://github.com/htmlhint/HTMLHint/wiki/Rules\n- https://htmlhint.com/docs/user-guide/list-rules';
-  readonly options = {
+    'Validate html files with htmlhint. \nRead about rules on:\n- https://github.com/htmlhint/HTMLHint/wiki/Rules\n- https://htmlhint.com/docs/user-guide/list-rules'
+  readonly builder = this.options({
     source: {
       alias: 'src',
       describe: 'Source path of the html files.',
-      type: 'string' as const,
-      default: 'public'
+      type: 'string',
+      default: 'public',
+    },
+  } as const)
+
+  constructor() {
+    super()
+  }
+
+  handler(args: ArgumentsCamelCase<InferredOptionTypes<typeof this.builder>>) {
+    const commandStartedAt = uptime()
+
+    const { source } = args
+    const files = File.sync(`${source}/**/*.+(html)`, { absolute: true })
+
+    if (!files.length) {
+      warn(this.command, 'HTML files not found')
+      exit(0)
     }
-  };
 
-  handler(yargs: Argv) {
-    return yargs.command(this.command, this.description, this.options, (args) => {
-      console.time(this.command);
+    /**
+     * DEBT: Can it be a factory of configs?
+     */
+    const htmlhintrc = File.find('.htmlhintrc')
+    const ruleSet: Ruleset = htmlhintrc.isFile()
+      ? JSON.parse(htmlhintrc.read())
+      : config.HTML_RULE_SET
 
-      const { source } = args;
-      const files = File.sync(`${source}/**/*.+(html)`, { absolute: true });
+    const lintResults = files
+      .map((file) => ({
+        fileContent: File.find(file).read(),
+        file,
+      }))
+      .map(({ fileContent, file }) =>
+        HTMLHint.verify(fileContent, ruleSet).map((lintResult: Hint) => ({
+          file,
+          error: lintResult,
+        })),
+      )
+      .filter((lintResult) => lintResult.length !== 0)
 
-      if (!files.length) {
-        warn(this.command, 'HTML files not found');
-        process.exit(0);
-      }
+    if (!lintResults.length) {
+      log(this.command, 'No html errors have been found :)')
+      return
+    }
 
-      /**
-       * DEBT: Can it be a factory of configs?
-       */
-      const htmlhintrc = File.find('.htmlhintrc');
-      const ruleSet: Ruleset = htmlhintrc.isFile() ? JSON.parse(htmlhintrc.read()) : config.HTML_RULE_SET;
+    for (const messages of lintResults) {
+      htmlhintStylish({
+        cwd: cwd(),
+        htmlhint: {
+          messages,
+        },
+      })
+    }
 
-      const lintResults = files
-        .map((file) => ({
-          fileContent: File.find(file).read(),
-          file
-        }))
-        .map(({ fileContent, file }) =>
-          HTMLHint.verify(fileContent, ruleSet).map((lintResult: Hint) => ({
-            file,
-            error: lintResult
-          }))
-        )
-        .filter((lintResult) => lintResult.length !== 0);
+    const commandFinishedAt = uptime()
+    const commandElapsedTime = commandFinishedAt - commandStartedAt
 
-      if (!lintResults.length) {
-        log(this.command, 'No html errors have been found :)');
-        return;
-      }
-
-      for (const messages of lintResults) {
-        htmlhintStylish({
-          cwd: cwd(),
-          htmlhint: {
-            messages
-          }
-        });
-      }
-      console.timeEnd(this.command);
-
-      Notify.info(this.command, 'Task done, watch results in console');
-    });
+    log(this.command, `${commandElapsedTime.toFixed(3)}s`)
+    Notify.info(this.command, 'Task done, watch results in console')
   }
 }
-const htmlValidate = new HtmlValidate();
-type HtmlValidateOptions = InferredOptionTypes<typeof htmlValidate.options>;
-
-export { htmlValidate, HtmlValidateOptions };
+const htmlValidate = new HtmlValidate()
+export { htmlValidate }
