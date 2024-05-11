7ce6c70802c3ffd2d49016c5f52511acfd31e5a5
Samuel R. <samuel_-_rojas@hotmail.com>
2024-05-10 22:30:02 -0500

    refactor(command): image minify

--- src/commands/images/image-minify.ts
+++ src/commands/images/image-minify.ts
@@ -1,135 +1,166 @@
-import { join, resolve } from 'path';
-import { error, success, warn } from '@/shared/helpers/console';
-import { File } from '@/shared/lib/file';
-import { Notify } from '@/shared/lib/notify';
+import { join, resolve } from 'node:path'
+import { exit } from 'node:process'
 
-import imageminPngquant from 'imagemin-pngquant';
-import imageminJpegtran from 'imagemin-jpegtran';
-import imageminZopfli from 'imagemin-zopfli';
-import { Argv, InferredOptionTypes, Options } from 'yargs';
-import { Command } from '@/shared/interfaces/command';
-import { exit } from 'process';
-const imageminGiflossy = require('imagemin-giflossy');
+import type { ArgumentsCamelCase, InferredOptionTypes } from 'yargs'
+import imageminPngquant from 'imagemin-pngquant'
+import imageminJpegtran from 'imagemin-jpegtran'
+import imageminZopfli from 'imagemin-zopfli'
+import imagemin from 'imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminGiflossy from 'imagemin-giflossy'
 
-class ImageMinify implements Command {
-  readonly command = 'img:minify';
+import imageminSvgo from '#@/src/shared/imagemin-svgo.js'
+import { error, success, warn } from '#@/src/shared/helpers/console.js'
+import { File } from '#@/src/shared/lib/file.js'
+import { Notify } from '#@/src/shared/lib/notify.js'
+import { YargsCommand } from '#@/src/shared/yargs-command.js'
 
-  readonly options = {
+class ImageMinify extends YargsCommand {
+  readonly command = 'img:minify'
+
+  readonly builder = this.options({
     source: {
       alias: 'src',
       describe: 'Source path without optimization.',
-      type: 'string' as const,
-      default: 'src/assets/img/src'
+      type: 'string',
+      default: 'src/assets/img/src',
     },
     distribution: {
       alias: 'dist',
       describe: 'Distribution path for optimized images.',
-      type: 'string' as const,
-      default: 'src/assets/img/dist'
-    }
-  };
+      type: 'string',
+      default: 'src/assets/img/dist',
+    },
+  } as const)
 
-  readonly description = 'Minify images';
+  readonly description = 'Minify images'
 
-  handler(yargs: Argv) {
-    yargs.command(this.command, this.description, this.options, async (args) => {
-      console.time(this.command);
-      await minify(args);
-      console.timeEnd(this.command);
-      Notify.info('Minify', 'Minify images task has ended');
-    });
+  async handler(args: ArgumentsCamelCase<InferredOptionTypes<typeof this.builder>>) {
+    console.time(this.command)
+    await minify(args)
+    console.timeEnd(this.command)
+    Notify.info('Minify', 'Minify images task has ended')
   }
 }
 
-async function minify({ source, distribution }: MinifyOptions) {
-  const { default: imagemin } = await import('imagemin');
-  const { default: imageminMozjpeg } = await import('imagemin-mozjpeg');
-  const { default: imageminSvgo } = await import('imagemin-svgo');
-
-  const src = File.find(source);
+async function minify({ source, distribution }: { source: string; distribution: string }) {
+  const src = File.find(source)
   if (!src.isDirectory()) {
-    error(imageMinify.command, `\nDirectory ${src.info.absolutePath} not found`);
-    exit(0);
+    error(imageMinify.command, `\nDirectory ${src.info.absolutePath} not found`)
+    exit(0)
   }
 
-  warn(imageMinify.command, 'Search in:', src.info.absolutePath);
-
-  const dist = File.find(distribution);
-  warn(imageMinify.command, 'Result in:', dist.info.absolutePath);
+  warn(imageMinify.command, 'Search in:', src.info.absolutePath)
+  const dist = File.find(distribution)
+  warn(imageMinify.command, 'Result in:', dist.info.absolutePath)
 
   // Notify.info('Minify', 'Start minify image task');
 
-  const files = File.sync('**/*.{png,jpeg,jpg,gif,svg}', { cwd: src.info.absolutePath, absolute: true }).map((file) => {
+  const files = File.sync('**/*.{png,jpeg,jpg,gif,svg}', {
+    cwd: src.info.absolutePath,
+    absolute: true,
+  }).map((filePath) => {
+    const file = File.find(filePath)
     // [input]: /dvx-demo-project/src/assets/img/src/webpack/webpack.png
     // [output]: /webpack/webpack.png
-    const distDir = File.find(file).info.dir.replace(src.info.absolutePath, '');
+    const distDir = file.info.dir.replace(src.info.absolutePath, '')
     // [input]: /webpack/webpack.png
     // [output]: /dvx-demo-project/src/assets/img/dist/webpack
     const destination =
       distDir.startsWith('\\') || distDir.startsWith('/')
         ? join(dist.info.absolutePath, distDir)
-        : resolve(dist.info.absolutePath, distDir);
+        : resolve(dist.info.absolutePath, distDir)
     return {
-      file,
-      destination
-    };
-  });
+      file: filePath,
+      destination,
+      ext: file.info.ext.toLocaleLowerCase(),
+    }
+  })
 
   for (const fileInfo of files) {
-    const { file, destination } = fileInfo;
+    const { file, destination, ext } = fileInfo
+    const plugins = [
+      ...(['.png'].includes(ext)
+        ? [
+            imageminPngquant({
+              speed: 1,
+              quality: [0.6, 0.8], //98 //lossy settings
+            }),
+            imageminZopfli({
+              more: true,
+            }),
+          ]
+        : []),
+      ...(['.gif'].includes(ext)
+        ? [
+            imageminGiflossy({
+              optimizationLevel: 3,
+              optimize: 3, //keep-empty: Preserve empty transparent frames
+              lossy: 2,
+            }),
+          ]
+        : []),
+      ...(['.svg'].includes(ext)
+        ? [
+            imageminSvgo({
+              plugins: [
+                {
+                  name: 'preset-default',
+                },
+                {
+                  name: 'removeViewBox',
+                  // @ts-ignore
+                  active: true,
+                },
+                {
+                  name: 'cleanupIds',
+                  // @ts-ignore
+                  active: false,
+                },
+                {
+                  name: 'sortAttrs',
+                  params: {
+                    xmlnsOrder: 'alphabetical',
+                  },
+                },
+              ],
+            }),
+          ]
+        : []),
+      ...(['.jpg', '.jpeg'].includes(ext)
+        ? [
+            imageminJpegtran({
+              progressive: true,
+            }),
+            imageminMozjpeg({
+              quality: 90,
+            }),
+          ]
+        : []),
+    ]
+
     try {
       const images = await imagemin([file], {
         destination,
-        plugins: [
-          imageminPngquant({
-            speed: 1,
-            quality: [0.6, 0.8] //98 //lossy settings
-          }),
-          imageminZopfli({
-            more: true
-          }),
-          imageminGiflossy({
-            optimizationLevel: 3,
-            optimize: 3, //keep-empty: Preserve empty transparent frames
-            lossy: 2
-          }),
-          imageminSvgo({
-            plugins: [
-              {
-                name: 'preset-default',
-                active: true
-              },
-              {
-                name: 'removeViewBox',
-                active: true
-              },
-              {
-                name: 'cleanupIDs',
-                active: false
-              }
-            ]
-          }),
-          imageminJpegtran({
-            progressive: true
-          }),
-          imageminMozjpeg({
-            quality: 90
-          })
-        ]
-      });
+        plugins,
+      })
       // Note: Extra process, evaluate
       // File.find(images[0].sourcePath).info.path
       // File.find(images[0].destinationPath).info.path
-      success(imageMinify.command, '\n[from]\t:', images[0].sourcePath, '\n[to]\t:', images[0].destinationPath);
+      success(
+        imageMinify.command,
+        '\n[from]\t:',
+        images[0]!.sourcePath,
+        '\n[to]\t:',
+        images[0]!.destinationPath,
+      )
       //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
     } catch (e) {
-      error(imageMinify.command, `${file}\n${e}`);
+      error(imageMinify.command, `${file}\n${e}`)
     }
   }
 }
 
-const imageMinify = new ImageMinify();
+const imageMinify = new ImageMinify()
 
-type MinifyOptions = InferredOptionTypes<typeof imageMinify.options>;
-
-export { imageMinify, MinifyOptions, minify };
+export { imageMinify, minify }
