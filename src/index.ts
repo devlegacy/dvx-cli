7b14833a24a5b02580dbd5cd727cce6acbaaa5b9
Samuel R. <samuel_-_rojas@hotmail.com>
2025-07-15 14:07:56 -0500

    feat(cli): implement yargs for command-line argument parsing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
 #!/usr/bin/env node
 
 import { argv } from 'node:process'
-import { DvxCLI } from './dvx.js'
 
-await new DvxCLI(argv).parse()
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+
+import { version, epilogue, usage, scriptName } from '#@/src/shared/domain/cli-metadata.js'
+
+const cli = yargs(hideBin(argv))
+
+// configure yargs
+cli
+  .epilogue(epilogue)
+  .commandDir('./commands', {
+    include: /.command\.js$/,
+    extensions: ['js'],
+    recurse: true,
+  })
+  .help('help', 'Show help', false)
+  .locale('en')
+  .scriptName(scriptName)
+  .usage(usage)
+  .wrap(95)
+  .version('version', 'Show current version number', version)
+  .hide('version')
+  .hide('help')
+  .strictCommands()
+
+/**
+ * DOC: argv, the letter v is an abbreviation of vector, arg - vector | arg - array
+ * Read more on: http://decsai.ugr.es/~jfv/ed1/c/cdrom/cap6/cap64.htm
+ */
+const args = await cli.parse() // args vector - without flags
+const argsCount = args._.length
+
+if (!argsCount) cli.showHelp()
