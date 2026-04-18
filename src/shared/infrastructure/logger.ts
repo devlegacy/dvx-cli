1fc73bf95c84983e605e20ab50a98a54cd9b6740
Samuel R. <samuel_-_rojas@hotmail.com>
2026-04-18 04:08:43 -0500

    refactor: streamline logger configuration and remove commented code

--- src/shared/infrastructure/logger.ts
+++ src/shared/infrastructure/logger.ts
@@ -1,45 +1,32 @@
-import { default as PinoPretty } from 'pino-pretty'
-import pino from 'pino'
-import process from 'process'
+import pino from 'pino';
+import { default as PinoPretty } from 'pino-pretty';
 
-export const MESSAGE_KEY = 'message'
-const level = 'trace'
-const stream = true
-  ? PinoPretty({
-      colorize: true,
-      ignore: 'pid,hostname',
-      levelFirst: true,
-      translateTime: 'yyyy-mm-dd HH:MM:ss',
-      // messageKey: MESSAGE_KEY,
-      level,
-    })
-  : process.stdout
+export const MESSAGE_KEY = 'message';
+const level = 'trace';
+const stream = PinoPretty({
+  colorize: true,
+  ignore: 'pid,hostname',
+  levelFirst: true,
+  translateTime: 'yyyy-mm-dd HH:MM:ss',
+});
 
-export const streams = [{ stream }]
-// console.log(streams)
+export const streams = [
+  {
+    stream,
+  },
+];
+
 /**
  * Read more on: https://getpino.io/#/
  */
 export const logger = pino.pino(
   {
     level,
-    // messageKey: MESSAGE_KEY,
     base: null,
   },
   pino.multistream(streams),
-)
+);
 
-export const info = logger.info.bind(logger)
-export const warn = logger.warn.bind(logger)
-export const error = logger.error.bind(logger)
-
-// info({ message: 'Hello, info' })
-info({ message: { data: [{ numbers: 100 }] } }, 'another message')
-info({ message: { data: [{ numbers: 100 }] } })
-// info('Hello, info')
-// warn({ message: 'Hello, warn' })
-// warn({ message: { data: [{ numbers: 100 }] } })
-// warn('Hello, warn')
-// error({ message: 'Hello, error' })
-// error({ message: { data: [{ numbers: 100 }] } })
-// error('Hello, error')
+export const info = logger.info.bind(logger);
+export const warn = logger.warn.bind(logger);
+export const error = logger.error.bind(logger);
